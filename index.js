8630c23a7aab21656bfce822812fff9a66625949
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-22 00:16:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function divide(a, b) {
 }
 
 function increment(n) {
-  return ++n;
+  return n++;
 }
 
 function decrement(n) {
-  return --n;
+  return n--;
 }
 
 function makeInt(n) {
-  return parseInt(n);
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n) {
